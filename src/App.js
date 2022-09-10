f7227796868a142da80706237a354af75dc3d363
Amir Musaev <a.musaev@centroidsol.com>
2022-09-11 01:08:58 +0400

    feat: exchanger

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import { Box, FormControl, Select, MenuItem, TextField } from '@mui/material';
+import { XMLParser } from 'fast-xml-parser'
+
+const parser = new XMLParser()
 
 function App() {
+  const [currencies, setCurrencies] = useState([]);
+  const [firstCurrency, setFirstCurrency] = useState('USD');
+  const [secondCurrency, setSecondCurrency] = useState('RUB');
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    fetch('https://www.cbr-xml-daily.ru/daily_utf8.xml')
+      .then(res => res.text())
+      .then(res => parser.parse(res))
+      .then(res => res.ValCurs.Valute)
+      .then(res => [{ CharCode: "RUB", Name: "Российский Рубль", Value: "1" }, ...res])
+      .then(setCurrencies)
+  }, [])
+  const firstCurrencyChangeHandler = (event) => {
+    setFirstCurrency(event.target.value)
+  }
+  const secondCurrencyChangeHandler = (event) => {
+    setSecondCurrency(event.target.value)
+  }
+  const valueChangeHandler = (event) => {
+    setValue(event.target.value)
+  }
+
+  const firstCurrencyValue = (+currencies
+    .find((currency => currency.CharCode === firstCurrency))
+    ?.Value
+    ?.replace(',', '.')) || 0
+  const secondCurrencyValue = (+currencies
+    .find((currency => currency.CharCode === secondCurrency))
+    ?.Value
+    ?.replace(',', '.')) || 0
+
+  const rubResult = value * firstCurrencyValue
+  const result = rubResult / secondCurrencyValue
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: 'flex', gap: 2, m: 5, width: 300 }}>
+      <Box>
+        <TextField fullWidth variant="standard" value={value} onChange={valueChangeHandler} />
+        <FormControl fullWidth>
+          <Select
+            variant='standard'
+            value={firstCurrency}
+            onChange={firstCurrencyChangeHandler}
+          >
+            {currencies.map(currency =>
+              <MenuItem dense key={currency.CharCode}
+                value={currency.CharCode}>
+                {currency.Name}
+              </MenuItem>
+            )}
+          </Select>
+        </FormControl>
+      </Box>
+      <Box>
+        <TextField fullWidth variant="standard" disabled value={result.toFixed(2)} />
+        <FormControl fullWidth>
+          <Select
+            variant="standard"
+            value={secondCurrency}
+            onChange={secondCurrencyChangeHandler}
+          >
+            {currencies.map(currency =>
+              <MenuItem dense key={currency.CharCode}
+                value={currency.CharCode}>
+                {currency.Name}
+              </MenuItem>
+            )}
+          </Select>
+        </FormControl>
+      </Box>
+    </Box>
   );
 }
 
